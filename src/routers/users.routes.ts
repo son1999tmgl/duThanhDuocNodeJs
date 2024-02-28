4c3bd0d867a844ebf7723165e282d4c566da9b5b
sonnx <sonnx@efy.com.vn>
2024-02-28 11:31:17 +0700

    87

--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { loginController, registerController } from '~/controllers/users.controllers';
 import { loginValidate, registerValidate } from '~/middlewares/users.middlewares';
-import { wrapAsync } from '~/utils/handlers';
+import { wrapRequestHandler } from '~/utils/handlers';
 const usesRouter = Router();
 
 /**
  * Description: Register a new user
  * Body: { name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601 }
  */
-usesRouter.post('/register', registerValidate, wrapAsync(registerController));
+usesRouter.post('/register', registerValidate, wrapRequestHandler(registerController));
 export default usesRouter;
--- src/utils/handlers.ts
+++ src/utils/handlers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
-export const wrapAsync = (func: RequestHandler) => {
+export const wrapRequestHandler = (func: RequestHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
       func(req, res, next);
